b87b086767c1f671747e0585a3ca6d0f29f9f5d3
Jesus Aguas <736935@unizar.es>
2019-12-09 19:10:32 -0800

    Initial commit

---
+++ iCollege/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCollege
+//
+//  Created by Jesús Aguas Acin on 09/12/2019.
+//  Copyright © 2019 Jesus Aguas Acin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
