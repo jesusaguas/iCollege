84fc7e66dbddc76583a60106b0842641290607d5
Aitor Sola <aitorsola@Macbook-Pro-de-Aitor.local>
2019-12-11 12:09:56 -0800

    Courses

---
+++ iCollege/Calendar/Animations.swift
@@ -0,0 +1,9 @@
+//
+//  Animations.swift
+//  iCollege
+//
+//  Created by Aitor Sola on 11/12/2019.
+//  Copyright © 2019 Jesus Aguas Acin. All rights reserved.
+//
+
+import Foundation
---
+++ iCollege/ViewControllers/CoursesPopUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CoursesPopUpViewController.swift
+//  iCollege
+//
+//  Created by Aitor Sola on 11/12/2019.
+//  Copyright © 2019 Jesus Aguas Acin. All rights reserved.
+//
+
+import UIKit
+
+class CoursesPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
